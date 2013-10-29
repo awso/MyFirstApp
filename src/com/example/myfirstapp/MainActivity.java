bb690676b56f0f375bf9313e6ddfb16fcd2f1b7b
awso <hi.yanma@gmail.com>
2013-10-28 22:54:47 -0700

    db support in Android app

---
+++ src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myfirstapp;
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+@Override
+
+public void onCreate(Bundle savedInstanceState) {
+
+super.onCreate(savedInstanceState);
+
+super.loadUrl("file:///android_asset/www/index.html");
+
+}
+
+}
